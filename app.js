f9a77306a5e65ddae6e4ad60da4971d36b808d12
Swapnil Tripathi <65762022+swapniltri@users.noreply.github.com>
2022-05-16 20:24:43 +0530

    my1 commit

--- app.js
+++ app.js
@@ -8,21 +8,44 @@ app.use(bodyParser.urlencoded({extended:true}));
 const ejs=require("ejs");
 app.set('view engine','ejs');
 
+//Config
+const dotenv = require("dotenv");
+dotenv.config({path:"./config.env"});
 
 const mongoose=require("mongoose");
 
-const DB = "mongodb+srv://swapnil-admin:test123@cluster0.fesmd.mongodb.net/peopleDB?retryWrites=true&w=majority";
+//Session Configuration
+const session = require("express-session");
+const MongoStore = require("connect-mongodb-session")(session); //Local storage on mongoDB atlas.
+
+const DB = process.env.DB_URL;
 mongoose.connect(DB,{useNewUrlParser:true,useUnifiedTopology:true}).then(() => {
     console.log("connection successful");
 }).catch((err) => console.log("No connection"));
 
+//Establishing the session connection with mongoDB, after connecting the application to mongoDB
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({
+        uri: DB,
+        collection: "session"
+    }),
+    cookie:{
+        maxAge: 1000 * 60 * 60 * 24 //1 day in milisec.
+    }
+}));
 
 const peopleSchema=new mongoose.Schema({
+    username:String,
     email:String,
     password:String
 });
 
 const secretsSchema=new mongoose.Schema({
+    user:String,
     heading:String,
     content:String
 });
@@ -30,79 +53,39 @@ const secretsSchema=new mongoose.Schema({
 const People=mongoose.model("People",peopleSchema);
 const Secrets=mongoose.model("Secrets",secretsSchema);
 
-// const mySecret1=new Secrets({
-//     heading:"My Secret",
-//     content:"I love to watch Cartoons"
-// });
+const isAuth = function(req,res,next){
+    if(req.session.isAuth){
+        next();
+    }else{
+        res.redirect("/secrets");
+    }
+}
 
-const mySecret2=new Secrets({
-    heading:"Codechef",
-    content:"4⭐ coder at Codechef."
-});
-
-const mySecret3=new Secrets({
-    heading:"Codeforces",
-    content:"Specialist on Codeforces"
-});
-
-const mySecret=[mySecret2,mySecret3];
-
-// mySecret1.save();
-// mySecret2.save();
-// mySecret3.save();
+const isLoggedIn = function(req,res,next){
+    if(req.session.isAuth){
+        res.redirect("/secrets");
+    }else{
+        next();
+    }
+}
 
 app.get("/",function(req,res){
     res.render("index");
 });
 
-app.get("/login",function(req,res){
+app.get("/login", isLoggedIn ,function(req,res){
     res.render("login",{signup:"",incpass:""});
 });
 
-app.get("/signup",function(req,res){
-    res.render("signup",{notRegistered:""});
-});
-
-app.get("/secrets",function(req,res){
-    Secrets.find({},function(err,foundArray){
-        if(foundArray.length === 1){
-            if(foundArray[0].heading === "Codechef"){
-                Secrets.insertMany([mySecret3],function(err){
-                    if(err){
-                        console.log(err);
-                    }else{
-                        res.redirect("/secrets");
-                    }
-                });
-            }else{
-                Secrets.insertMany([mySecret2],function(err){
-                    if(err){
-                        console.log(err);
-                    }else{
-                        res.redirect("/secrets");
-                    }
-                });
-            }
-            // Secrets.insertMany(mySecret,function(err){
-            //     if(err){
-            //         console.log(err);
-            //     } else {
-            //         res.redirect("/secrets");
-            //     }
-            // });
-        } else {
-            res.render("secrets",{array:foundArray});
-        }
-    });
-});
-
 app.post("/login",function(req,res){
-    const email=req.body.email;
+    const username=req.body.username;
     const password=req.body.password;
-    People.findOne({email:email},function(err,foundUser){
+    People.findOne({username:username},function(err,foundUser){
         if(foundUser){
             if(foundUser.password === password){
-                res.redirect("secrets");
+                req.session.username = username;
+                req.session.isAuth = true;
+                res.redirect("/secrets");
             } else {
                 res.render("login",{incpass:"! Incorrect Password" , signup:""});
             }
@@ -112,23 +95,62 @@ app.post("/login",function(req,res){
     });
 });
 
+app.get("/signup",function(req,res){
+    res.render("signup",{notRegisteredU:"",notRegisteredE:""});
+});
+
 app.post("/signup",function(req,res){
-    People.findOne({email:req.body.email},function(err,foundEmail){
-        if(!foundEmail){
-            const newUser=new People({
-                email: req.body.email,
-                password: req.body.password
-            });
-            newUser.save();
-            res.redirect("secrets");
-        } else {
-            res.render("signup",{notRegistered:"! This email is already registered"});
+    const username = (req.body.username).trim();
+    const email = req.body.email;
+    const password = req.body.password;
+    People.findOne({username: req.body.username},function(err,foundUser){
+        if(err){
+            console.log(err);
+        }else{
+            if(!foundUser){
+                People.findOne({email: req.body.email},function(err,foundMail){
+                    if(err){
+                        console.log(err);
+                    }else{
+                        if(!foundMail){
+                            req.session.username = username;
+                            req.session.isAuth = true;
+                            const newUser=new People({
+                                username: username,
+                                email: email,
+                                password: password
+                            });
+                            newUser.save();
+                            res.redirect("/secrets");
+                        }else{
+                            res.render("signup",{notRegisteredU:"", notRegisteredE:"This email is already in use"});
+                        }
+                    }
+                });
+            }else{
+                res.render("signup",{notRegisteredU:"User already exist", notRegisteredE:""});
+            }
         }
     });
 });
 
-app.post("/secrets",function(req,res){
+app.get("/secrets", isAuth , function(req,res){
+    Secrets.find({},function(err,foundArray){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("secrets",{array:foundArray});
+        }
+    });
+});
+
+
+
+
+
+app.post("/secrets", isAuth ,function(req,res){
     const putSecrets=new Secrets({
+        user:req.session.username,
         heading:req.body.head,
         content:req.body.body
     });
@@ -136,16 +158,26 @@ app.post("/secrets",function(req,res){
     res.redirect("/secrets");
 });
 
-app.post("/delete",function(req,res){
-    const title=req.body.hiddenInput;
-    Secrets.deleteOne({heading:title},function(err){
-        if(err){
-            console.log(err);
-        } else {
-            console.log("Deleted Successfully");
-        }
-    });
+app.post("/delete", isAuth ,function(req,res){
+    const title=req.body.hiddenInput2;
+    const user=req.body.hiddenInput1;
+    if(req.session.username === user){
+        Secrets.deleteOne({heading:title},function(err){
+            if(err){
+                console.log(err);
+            } else {
+                console.log("Deleted Successfully");
+            }
+        });
+    }
     res.redirect("/secrets");
+});
+
+app.get("/signout", isAuth ,function(req,res){
+    req.session.destroy(err => {
+        console.log(err);
+    });
+    res.redirect("/");
 });
 
 app.listen(process.env.PORT || 3000,function(){
